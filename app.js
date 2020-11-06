a753e3d1631ded0c567c97445551ecbaf4a1dbfa
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-11-05 19:03:42 -0500

    LocalStorage

--- app.js
+++ app.js
@@ -51,6 +51,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 };
 
+// Delete Book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById('title').value = '';
@@ -58,7 +65,7 @@ UI.prototype.clearFields = function () {
   document.getElementById('isbn').value = '';
 };
 
-// Event Listeners
+// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   e.preventDefault();
 
@@ -87,4 +94,18 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     // Clear fields
     ui.clearFields();
   }
+});
+
+// Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function (e) {
+  //Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success');
+
+  e.preventDefault();
 });
